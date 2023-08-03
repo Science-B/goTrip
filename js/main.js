012fbd17e14c0f533b122f91f03d563dbc75eb35
Science-B <dmitriytwd@gmail.com>
2023-08-03 15:06:37 +0300

    added mobile-verison

--- js/main.js
+++ js/main.js
@@ -4,6 +4,16 @@ owl.owlCarousel({
   loop: true,
   margin: 30,
   items: 3,
+  responsive: {
+    850: { items: 2 },
+    1000: {
+      margin: 20,
+      items: 3,
+    },
+    1200: {
+      margin: 30,
+    },
+  },
 });
 $(".slider__btn--next").click(function () {
   owl.trigger("next.owl.carousel", [300]);
@@ -11,3 +21,13 @@ $(".slider__btn--next").click(function () {
 $(".slider__btn--prev").click(function () {
   owl.trigger("prev.owl.carousel");
 });
+
+const navBtn = document.querySelector(".nav__toggle");
+const nav = document.querySelector(".nav");
+const menuIcon = document.querySelector(".menu-icon");
+
+navBtn.onclick = function () {
+  nav.classList.toggle("nav--mobile");
+  menuIcon.classList.toggle("menu-icon--active");
+  document.body.classList.toggle("no-scroll");
+};
